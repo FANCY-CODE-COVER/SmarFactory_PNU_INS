30f638518d1464185ff1e46ad47b58027aefd825
Seo-GwangHyeon <dya2122@gmail.com>
2020-08-10 16:02:37 +0900

    카카오 로그인만 가능하도록 개발

--- Server_Project/Client_For_Server_Test/app/src/main/java/com/pnu/spring/smartfactory/client_for_server_test/MainActivity.java
+++ Server_Project/Client_For_Server_Test/app/src/main/java/com/pnu/spring/smartfactory/client_for_server_test/MainActivity.java
@@ -1,21 +1,61 @@
 package com.pnu.spring.smartfactory.client_for_server_test;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import com.kakao.auth.*;
+import com.kakao.util.exception.KakaoException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // 세션 콜백 구현
+    private ISessionCallback sessionCallback = new ISessionCallback() {
+        @Override
+        public void onSessionOpened() {
+            Log.i("KAKAO_SESSION", "로그인 성공");
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            Log.e("KAKAO_SESSION", "로그인 실패", exception);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         Button button= (Button) findViewById(R.id.button) ;
         button.setOnClickListener(this);
+        // 세션 콜백 등록
+        Session.getCurrentSession().addCallback(sessionCallback);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // 세션 콜백 삭제
+        Session.getCurrentSession().removeCallback(sessionCallback);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        // 카카오톡|스토리 간편로그인 실행 결과를 받아서 SDK로 전달
+        if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
+            return;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 
@@ -28,4 +68,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             btn.setText("abcd");
         }
     }
+
 }

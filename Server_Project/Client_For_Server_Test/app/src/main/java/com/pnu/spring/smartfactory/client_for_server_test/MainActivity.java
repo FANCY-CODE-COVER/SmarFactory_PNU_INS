b3eefe1a832e7dd20a87fe60991b4bdec8f35b58
Seo-GwangHyeon <dya2122@gmail.com>
2020-08-18 17:53:08 +0900

    안드로이드 클라이언트 커밋

--- Server_Project/Client_For_Server_Test/app/src/main/java/com/pnu/spring/smartfactory/client_for_server_test/MainActivity.java
+++ Server_Project/Client_For_Server_Test/app/src/main/java/com/pnu/spring/smartfactory/client_for_server_test/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pnu.spring.smartfactory.client_for_server_test;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import androidx.annotation.Nullable;
@@ -26,6 +27,16 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.*;
 import com.kakao.auth.*;
+import com.kakao.auth.network.response.AccessTokenInfoResponse;
+import com.kakao.kakaotalk.callback.TalkResponseCallback;
+import com.kakao.kakaotalk.v2.KakaoTalkService;
+import com.kakao.message.template.LinkObject;
+import com.kakao.message.template.TemplateParams;
+import com.kakao.message.template.TextTemplate;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
+import com.kakao.usermgmt.callback.UnLinkResponseCallback;
 import com.kakao.util.exception.KakaoException;
 import com.pnu.spring.smartfactory.client_for_server_test.DAO.Category;
 import com.pnu.spring.smartfactory.client_for_server_test.DAO.DataDAO;
@@ -39,7 +50,7 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-     Button button;
+     Button button, btnLogout, btnUnlink, btnData;
      EditText editIPAddr;
      EditText editCategroyId;
      TextView textView;
@@ -64,33 +75,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         setContentView(R.layout.activity_main);
 
-        button= (Button) findViewById(R.id.button) ;
+        button= (Button) findViewById(R.id.btn_message) ;
         button.setOnClickListener(this);
+        btnLogout= (Button) findViewById(R.id.btn_logout) ;
+        btnLogout.setOnClickListener(this);
+        btnUnlink= (Button) findViewById(R.id.btn_unlink) ;
+        btnUnlink.setOnClickListener(this);
+        btnData =(Button) findViewById(R.id.btn_data) ;
+        btnData.setOnClickListener(this);
         editIPAddr = (EditText) findViewById(R.id.edit_ipaddr) ;
         editCategroyId= (EditText) findViewById(R.id.edit_category_id) ;
         textView=(TextView) findViewById(R.id.textView);
         // 세션 콜백 등록
         Session.getCurrentSession().addCallback(sessionCallback);
-
         networkService = MyApplication.getInstance().getNetworkService();
-
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
         // 세션 콜백 삭제
         Session.getCurrentSession().removeCallback(sessionCallback);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-
         // 카카오톡|스토리 간편로그인 실행 결과를 받아서 SDK로 전달
         if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
             return;
         }
-
         super.onActivityResult(requestCode, resultCode, data);
     }
 
@@ -98,28 +110,80 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        if (id == R.id.button)
-        {
+        if (id ==R.id.btn_data){
             requestDatas();
+        }
+        else if (id == R.id.btn_message) {
+            // 메시지 나에게 보내기 안드로이드 버전
+            AuthService.getInstance()
+                    .requestAccessTokenInfo(new ApiResponseCallback<AccessTokenInfoResponse>() {
+                        @Override
+                        public void onSessionClosed(ErrorResult errorResult) {
+                            Log.e("KAKAO_API", "세션이 닫혀 있음: " + errorResult);
+                        }
+
+                        @Override
+                        public void onFailure(ErrorResult errorResult) {
+                            Log.e("KAKAO_API", "토큰 정보 요청 실패: " + errorResult);
+                        }
+
+                        @Override
+                        public void onSuccess(AccessTokenInfoResponse result ) {
+                            Log.i("KAKAO_API", "사용자 아이디: " + result.toString());
+                            Log.i("KAKAO_API", "사용자 아이디: " + result.getUserId());
+                            Log.i("KAKAO_API", "남은 시간(s): " + result.getExpiresIn());
+                            ;
+                        }
+                    });
+        }//end btn message
+        else if (id == R.id.btn_logout){
+            UserManagement.getInstance()
+                    .requestLogout(new LogoutResponseCallback() {
+                        @Override
+                        public void onCompleteLogout() {
+                            Log.i("KAKAO_API", "로그아웃 완료");
+                        }
+                    });
+        }
+        else if (id == R.id.btn_unlink){
+            UserManagement.getInstance()
+                    .requestUnlink(new UnLinkResponseCallback() {
+                        @Override
+                        public void onSessionClosed(ErrorResult errorResult) {
+                            Log.e("KAKAO_API", "세션이 닫혀 있음: " + errorResult);
+                        }
+
+                        @Override
+                        public void onFailure(ErrorResult errorResult) {
+                            Log.e("KAKAO_API", "연결 끊기 실패: " + errorResult);
+
+                        }
+                        @Override
+                        public void onSuccess(Long result) {
+                            Log.i("KAKAO_API", "연결 끊기 성공. id: " + result);
+                        }
+                    });
         }
     }
 
-    public void requestDatas(){
-        Category join = new Category("A01");
-
+    public void requestDatas() {// 신신사 서버에서 데이터 받아오기
+        Category join = new Category("A01"); // A01 인 자료 요청
         Call<List<DataDAO>> joinContentCall=networkService.getDatas(join);
-
         joinContentCall.enqueue(new Callback<List<DataDAO>>(){
-
+            @SuppressLint("SetTextI18n")
             @Override
             public void onResponse(Call<List<DataDAO>> call, Response<List<DataDAO>> response) {
                 if(response.isSuccessful()){
 
                    List<DataDAO> dataDAO=response.body();
-                    textView.setText( dataDAO.get(0).getCode());
-
+                    assert dataDAO != null;
+                    StringBuilder result= new StringBuilder();
+                    for(int i = 0; i<dataDAO.size(); ++i) {
+                        result.append(dataDAO.get(i).getCode()).append("\n\r");
+                   }
+                    textView.setText(result.toString());
                 }
-                else{
+                else{// 실패시 에러코드들
                     if(response.code()==500)
                     {
 
@@ -134,12 +198,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     }
                 }
             }
-
             @Override
             public void onFailure(Call<List<DataDAO>> call, Throwable t) {
                 //실패
             }
         });
     }
-
 }

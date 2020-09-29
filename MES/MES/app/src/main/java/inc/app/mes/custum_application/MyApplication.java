6569181c8e0ffef04215a12a638d75c8c60775b6
Seo-GwangHyeon <dya2122@gmail.com>
2020-09-29 10:47:16 +0900

    git ignore 수정 및 수정사항 반영

--- MES/MES/app/src/main/java/inc/app/mes/custum_application/MyApplication.java
+++ MES/MES/app/src/main/java/inc/app/mes/custum_application/MyApplication.java
@@ -25,7 +25,7 @@ public class MyApplication extends Application {
         super.onCreate();
         MyApplication.instance = this;
         changed=false;
-        baseUrl="http://192.168.0.2:8082/smartfactory/";
+        baseUrl="";
         Log.i("BASE_URL", baseUrl);
         buildNetworkService();
 
